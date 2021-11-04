368f33aa06e4118e22cede3a687199a632ce3eb5
KarinaRV <karinaricioni39@gmail.com>
2021-11-03 23:53:09 -0300

    Atualização do CRUD

--- blogpessoal/blogpessoal/src/main/java/br/com/blogpessoal/model/Postagem.java
+++ blogpessoal/blogpessoal/src/main/java/br/com/blogpessoal/model/Postagem.java
@@ -6,35 +6,42 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 @Entity
 @Table(name = "postagem")
 
 public class Postagem {
-	
+
 	@Id
-	@GeneratedValue (strategy = GenerationType.IDENTITY)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long id;
-	
+
 	@NotNull
-	@Size(min = 5, max = 100)
-	private String titulo; 
-	
+	@Size(min = 5, max = 100, message = "O texto deve conter o mínimo de 5 caracteres e o máximo de 100")
+	private String titulo;
+
 	@NotNull
-	@Size(min = 10, max = 500)
+	@Size(min = 10, max = 500, message = "O texto deve conter o mínimo de 5 caracteres e o máximo de 500")
 	private String texto;
-	
+
 	@Temporal(TemporalType.TIMESTAMP)
 	private Date date = new java.sql.Date(System.currentTimeMillis());
 
 	public long getId() {
 		return id;
 	}
+	
+	@ManyToOne
+	@JsonIgnoreProperties("postagem")
+	private Tema tema;
 
 	public void setId(long id) {
 		this.id = id;
@@ -63,9 +70,13 @@ public class Postagem {
 	public void setDate(Date date) {
 		this.date = date;
 	}
-	
-	
-	
-	
+
+	public Tema getTema() {
+		return tema;
+	}
+
+	public void setTema(Tema tema) {
+		this.tema = tema;
+	}
 
 }
